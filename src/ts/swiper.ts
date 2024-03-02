448a5ba8c135a324bb2aafed1fa063e5a8f9f280
Anar Agaev <anar.n.agaev@gmail.com>
2024-03-02 21:42:27 +0300

    Fixed main potfolio slider

--- src/ts/swiper.ts
+++ src/ts/swiper.ts
@@ -4,15 +4,20 @@ import 'swiper/css'
 const initSwiper = (): void => {
 
     new Swiper('.swiper', {
-        slidesPerView: "auto",
+        direction: 'horizontal',
+        slidesPerView: 'auto',
         // spaceBetween: 36,
         loop: false,
         observer: true,
         observeParents: true,
         observeSlideChildren: true,
         watchOverflow: true,
+        grabCursor: true,
+        mousewheel: true,
 
-
+        // freeMode: {
+        //     enabled: true
+        // },
 
         // And if we need scrollbar
         scrollbar: {
