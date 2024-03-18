77b0632bb680ac91a039d54581afedb9c2e08e35
Anar Agaev <anar.n.agaev@gmail.com>
2024-03-18 15:13:13 +0300

    Added window blocker

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,4 @@
+import initWindowBlocker from './blocker'
 import initNav from './nav'
 import initPortfolioSwiper from './portfolio'
 import {initProjects, initProjectSwipers} from './project'
@@ -5,6 +6,7 @@ import {initProjects, initProjectSwipers} from './project'
 import 'swiper/swiper-bundle.css'
 
 window.addEventListener('load', () => {
+    initWindowBlocker()
     initNav()
     initPortfolioSwiper()
     initProjects()
